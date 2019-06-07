78cb3505320fea6e1a6f27a1d89cb9a893bf4b55
AndreinaKj <anoherli@gmail.com>
2019-06-07 09:31:55 -0500

    Avances: cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,13 @@
 window.cipher = {
   encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
-  },
-  decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
+
+let solved = " ";
+for(let i = 0;i<string.length; i++){
+  const asciNum = string[i].charCodeAt();
+  if(asciNum >= 65 && asciNum <= 90);
+  const code = ((asciNum - 65) + (parseInt(offset))) % 26 + 65;
+  solved += String.fromCharCode(code);
+  return solved
+}
   }
-};
+    };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const finalPage = document.getElementById("finalPage");
 const errorPage = document.getElementById("errorPage");
 const cifrar = document.getElementById("cifrar");
 const descifrar = document.getElementById("descifrar");
+const passwError = document.getElementById("passwError");
+const number = document.getElementById("number");
+const message = document.getElementById("message");
+const inicio = document.getElementById("inicio");
 
 //Definir variable para realizar función 
 const passwordTrue = "LABORATORIA";
@@ -19,12 +23,13 @@ const password = document.getElementById("password");
 //Verificar si la clave es incorrecta 3 veces
     } else {
         if (nroIntentos < 2) {
-            console.log("Clave incorrecta");
+            passwError.classList.replace("hide", "show");
             password.value = "";
             nroIntentos++; 
 //En caso la contraseña sea incorrecta, mostrar la página de error.
         } else {
             login.classList.add("hide");
+            passwError.classList.add("hide");
             errorPage.classList.replace("hide","show");
         }
     }
@@ -35,6 +40,32 @@ const password = document.getElementById("password");
 botonEntrar.addEventListener("click", validar);
 
 //Agregar funcionalidad al boton cifrar. Enlace a la página final.
+cifrar.addEventListener("click", () => {
+    centralPage.classList.add("hide");
+    finalPage.classList.replace("hide", "show" );
+})
+
+//Agregar funcionalidad al boton descifrar. Enlace a la página final.
+descifrar.addEventListener("click", () => {
+    centralPage.classList.add("hide");
+    finalPage.classList.replace("hide", "show" );
+})
+
+//Agregar funcionalidad al boton inicio. Enlace a la página final.
+inicio.addEventListener("click", () => {
+    finalPage.classList.add("hide");
+    centralPage.classList.replace("hide", "show" );
+})
+
+//Crear evento para que sea mostrado el resultado al momento de hacer click en el botón. 
+cifrar.addEventListener("click", () => {
+    const mensaje2 = window.cipher.encode(number.value,message.value);
+    finalMessage.value = mensaje2;
+}); 
+
+//Copiar en portapapeles los datos contenidos en el input de resultado de cifrado
+
+
 
 
 /* const boton = document.getElementById("botoncito");
