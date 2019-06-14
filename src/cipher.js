fff5db491ea415c211261d866b5fb0b2b1e61ff0
AndreinaKj <anoherli@gmail.com>
2019-06-13 23:54:07 -0500

    Reto Uno finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,32 @@
 window.cipher = {
-  encode: (number, message2) => {
+  encode: (offset, string) => {
+    let solved = "";
+    for (let i = 0; i < string.length; i++) {
+      const asciNum = string[i].charCodeAt();
+      if (asciNum >= 65 && asciNum <= 90) {
+        const code = ((asciNum - 65) + (parseInt(offset))) % 26 + 65;
+        solved += String.fromCharCode(code);
+      } else if (asciNum === 32) {
+        solved += " ";
+      }
+      
+    }
+    return solved; 
+  },
 
-let solved = " ";
-for(let i = 0;i<message2.length; i++){
-  const asciNum = message2[i].charCodeAt();
-  if(asciNum >= 65 && asciNum <= 90);{
-  const code = ((asciNum - 65) + (parseInt(number))) % 26 + 65;
-  solved += String.fromCharCode(code);
-}
+  decode: (offset, string) => {
+
+  let solved = "";
+  for (let i = 0; i < string.length; i++) {
+    const asciNum = string[i].charCodeAt();
+    if (asciNum >= 65 && asciNum <= 90){
+      const code = ((asciNum + 65) - (parseInt(offset))) % 26 + 65;
+      solved += String.fromCharCode(code);
+    } else if (asciNum === 32) {
+      solved += " ";
+    }
   }
   return solved;
-    },
-      
-  decode: (number, lastMessage) => {
-  
-   let solved = " ";
-   for(let i = 0;i<lastMessage.length; i++){
-     const asciNum = lastMessage[i].charCodeAt();
-     if(asciNum >= 65 && asciNum <= 90);{
-     const code = ((asciNum + 65) - (parseInt(number))) % 26 + 65;
-     solved += String.fromCharCode(code);
-   }
-     }
-     return solved;
-    
-       }      
-        };
+
+}      
+  };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,16 +10,34 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
-  });
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
 
+    it('debería dar indicar un espacio con offset de 33', () => {
+      assert.equal(cipher.encode(33, " "), " ");
+    });  
+
+});
+
+    it("Debe retornar H cuando ingrese A con offet 33", () => {
+      assert.equal(cipher.encode(33, "A"), "H")
+    });
+    
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");  
+    });
+
+    it('debería retornar un espacio con offset 33', () => {
+      assert.equal(cipher.decode(33, " "), " ");  
+    });
+
   });
 
 });
